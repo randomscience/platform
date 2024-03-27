541c454f31827f3aec6a2ae1d794079f02d5c133
Mateusz Kojro <mateuszkojro@outlook.com>
2024-03-27 07:32:16 +0100

    Remove unneeded imports

--- main.go
+++ main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"log"
-	"math"
-	"net/http"
-	"net/url"
 	"os"
-	"strconv"
-	"time"
 
-	"github.com/labstack/echo/v5"
-	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
-	"github.com/pocketbase/pocketbase/forms"
-	"github.com/pocketbase/pocketbase/models"
 )
 
 func main() {
